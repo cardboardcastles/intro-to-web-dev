a98847188603e398f13c13c04e510c46d6bb5b4c
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-02-18 18:54:45 +0000

    done!

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -11,9 +11,9 @@ function fillOutputs() {
  
   let outputDiv = document.getElementById('output');
   outputDiv.style.display = 'block';
-
+}
   
-  function showGreeting() {
+function showGreeting() {
 
   
   let adjectiveTwoInput = document.getElementById('adjective-two-input');
@@ -23,11 +23,64 @@ function fillOutputs() {
   adjectiveTwoOutput.innerText = adjectiveTwo;
 
   
-  let outputDiv = document.getElementById('output');
+  let outputDiv = document.getElementById('output2');
   outputDiv.style.display = 'block';
 
-  
+}
 
+
+function ThisOne() {
+
+  
+  let adjectiveThreeInput = document.getElementById('adjective-three-input');
+  let adjectiveThree = adjectiveThreeInput.value;
+
+  let adjectiveThreeOutput = document.getElementById('adjective-three-output');
+  adjectiveThreeOutput.innerText = adjectiveThree;
+
+  
+  let outputDiv = document.getElementById('output3');
+  outputDiv.style.display = 'block';
 
 }
+
+
+
+function ChaiLatte() {
+
+  
+  let adjectiveFourInput = document.getElementById('adjective-four-input');
+  let adjectiveFour = adjectiveFourInput.value;
+
+  let adjectiveFourOutput = document.getElementById('adjective-four-output');
+  adjectiveFourOutput.innerText = adjectiveFour;
+
+  
+  let outputDiv = document.getElementById('output4');
+  outputDiv.style.display = 'block';
+
+}
+
+
+
+function Green() {
+
+  
+  let adjectiveFiveInput = document.getElementById('adjective-five-input');
+  let adjectiveFive = adjectiveFiveInput.value;
+
+  let adjectiveFiveOutput = document.getElementById('adjective-five-output');
+  adjectiveFiveOutput.innerText = adjectiveFive;
+
+  
+  let outputDiv = document.getElementById('output5');
+  outputDiv.style.display = 'block';
+
+}
+function surprise() {
+
+let labelElement = document.getElementById('output6');
+labelElement.innerHTML = 'We will actually use that one later. What I bet you did not expect, is that these silly things you have been typing tell another story. <p>The result is below, enjoy the consequences of your actions:</p>';
+
+
 }
