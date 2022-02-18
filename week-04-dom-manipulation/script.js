2fecfa01e0011e794e5b74a0d943929a56461c16
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-02-18 16:25:05 +0000

    thank you samir absolute goat

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -1,37 +1,32 @@
 
-// When this function is called, it takes the values from the inputs and adds
-// them to the outputs.
 function fillOutputs() {
 
-  // Get the adjective input element
   let adjectiveOneInput = document.getElementById('adjective-one-input');
-  // Get the text from the input element
   let adjectiveOne = adjectiveOneInput.value;
 
-  // Get the adjective output element
+  
   let adjectiveOneOutput = document.getElementById('adjective-one-output');
-  // Set the text of the input element
   adjectiveOneOutput.innerText = adjectiveOne;
 
-  // Get the output container element
+ 
   let outputDiv = document.getElementById('output');
-  // Make the output container element visible
   outputDiv.style.display = 'block';
 
+  
+  function showGreeting() {
 
-function showGreeting() {
-let adjectiveTwoInput = document.getElementById ('adjective-two-input');
+  
+  let adjectiveTwoInput = document.getElementById('adjective-two-input');
+  let adjectiveTwo = adjectiveTwoInput.value;
 
-let adjectiveTwo = adjectiveTwoInput.value;
+  let adjectiveTwoOutput = document.getElementById('adjective-two-output');
+  adjectiveTwoOutput.innerText = adjectiveTwo;
 
-let adjectiveTwoOutput = document.getElementById ('adjective-two-output');
-adjectiveTwoOutput.innerText = adjectiveTwo
+  
+  let outputDiv = document.getElementById('output');
+  outputDiv.style.display = 'block';
 
-let outputDiv = document.getElementById('output2');
-outputDiv.style.display = 'block';
- 
-
-
+  
 
 
 }
