3ac428be1fa5149587ff87aa81e0c0e98cc431de
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-02-18 05:57:20 +0000

    i am going on my joker arc

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -4,17 +4,35 @@
 function fillOutputs() {
 
   // Get the adjective input element
-  const adjectiveOneInput = document.getElementById('adjective-one-input');
+  let adjectiveOneInput = document.getElementById('adjective-one-input');
   // Get the text from the input element
-  const adjectiveOne = adjectiveOneInput.value;
+  let adjectiveOne = adjectiveOneInput.value;
 
   // Get the adjective output element
-  const adjectiveOneOutput = document.getElementById('adjective-one-output');
+  let adjectiveOneOutput = document.getElementById('adjective-one-output');
   // Set the text of the input element
   adjectiveOneOutput.innerText = adjectiveOne;
 
   // Get the output container element
-  const outputDiv = document.getElementById('output');
+  let outputDiv = document.getElementById('output');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+
+
+function showGreeting() {
+let adjectiveTwoInput = document.getElementById ('adjective-two-input');
+
+let adjectiveTwo = adjectiveTwoInput.value;
+
+let adjectiveTwoOutput = document.getElementById ('adjective-two-output');
+adjectiveTwoOutput.innerText = adjectiveTwo
+
+let outputDiv = document.getElementById('output2');
+outputDiv.style.display = 'block';
+ 
+
+
+
+
+}
 }
