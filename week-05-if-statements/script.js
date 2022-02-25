fa67cb9d1f023f63f8015c47fbec10776b70b816
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-02-25 04:46:49 +0000

    up to question 3 fully working, question-four created

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -13,7 +13,7 @@ function submitAnswerOne() {
   } else if(answerOne == 'a number') {
     alert('oh, how clever. Get a calculator and try again.');
   } else {
-    alert('...try again.')
+    alert('...try again.');
   }
 }
 
@@ -23,18 +23,65 @@ function submitAnswerTwo() {
   // Get answer two's input element
   const answerTwoInput = document.getElementById('answer-two-input');
   // Get the number from answer two's input element
-  const answerTwo = answerTwoInput.value;
+   const answerTwo = answerTwoInput.value;
 
   if(answerTwo == "May 2nd, 1972") {
     alert("That's right!");
     hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
+    show('answer-three');
+  } else if(answerTwo == "May 2nd 1972") {
+    alert("That's right!");
+    hide('question-two');
+    show('question-three');
   } else {
-    alert('The answer is lower.')
+    alert('The answer is lower.');
   }
 }
+
+function submitAnswerThree() {
+  // Get answer two's input element
+  const answerThreeInput = document.getElementById('answer-three-input');
+  // Get the number from answer two's input element
+  const answerThree = answerThreeInput.value;
+
+  if(answerThree == "goat") {
+    alert("That's...correct! Congrats! What a speedrunner you are =D");
+    hide('question-three');
+    show('done');
+  } else if(answerThree == "leaf sheep") {
+    alert("Hey, that's my boyfriend's favorite animal! But still, no.");
+    hide('question-three');
+    show('question-four');
+  } else {
+    alert("That's a good animal, but no.");
+    hide('question-three');
+    show('question-four');
+  }
+}
+
+function submitAnswerFour() {
+  // Get answer one's input element
+  const answerFourInput = document.getElementById('answer-four-input');
+  // Get the text from answer one's input element
+  const answerFour = answerFourInput.value;
+
+  if(answerFour == '') {
+    alert("yes");
+    hide('question-four');
+    show('question-five');
+  } else if(answerFour == '') {
+    alert('oh, how funny and clever you are. Try that again.');
+  } else {
+    alert('...try again.');
+  }
+}
+
+
+
+
+
+
+
 
 // Helper function that takes an ID and shows the element with that ID.
 function show(id) {
