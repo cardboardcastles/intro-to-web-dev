e6ffc9f3f5af8ca00e33d11a731ea59555e3e74d
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-03-20 01:55:10 +0000

    week 6 complete?

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -19,12 +19,12 @@ Where are we gonna meet, I’m in the Holy Land right now can you come here quic
 Yeah, Badboyhalo, I bet you’re athletic
 ‘Cause you can run real, really fast
 Meet me at the Holy Land, and help me find some netherite
-Why you got lice in your head
+Yo, why you got lice in your head
 Why you got lice in your head`;
 
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
-const speechPunctuationRemoved = speech.replace(',', '').replace('.', '').replace('?','');
+const speechPunctuationRemoved = speech.replace(',', '').replace('.', '').replace('?','').replace("'"," ");
 
 // Use a regular expression to split the speech into individual words. You
 // shouldn't need to change this, unless you want to split on characters other
@@ -48,18 +48,35 @@ function displayLongWords() {
 }
 
 function displayNameStart() {
-  const NameStart = document.getElementById('NameStart');
+  const NameStartElement = document.getElementById('NameStart');
 
   for(let i = 0; i < wordsArray.length; i++) {
     const word = wordsArray[i];
 
-    if(word.startsWith ('C') || word.startsWith ('c')) {
+    if(word.startsWith ('C') || word.startsWith ('c') || word.startsWith ("'C")) {
       const wordElement = document.createElement('li');
        wordElement.innerText = word;
        NameStartElement.appendChild(wordElement);
     }
   }
 }
+
+
+function displayUncommonWords() {
+
+  let commonWords = ['Badboyhalo', 'why', 'in', 'your', 'head', 'you', 'got', 'lice', 'my', 'yo', 'My', 'Yo,'];
+   let uncommonWordsElement = document.getElementById('uncommon-words');
+
+  for(let word of wordsArray){
+
+    if(!commonWords.includes(word)){
+      const wordElement = document.createElement('li');
+       wordElement.innerText = word;
+       uncommonWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
 
 function displayLongestWord() {
   const longestWordElement = document.getElementById('longest-word');
@@ -79,9 +96,20 @@ function displayLongestWord() {
    longestWordElement.appendChild(wordElement);
 }
 
-function EveryThird() { document.getElementById('speech');
-  
+
+function EveryThird() { 
+let EveryThirdElement = document.getElementById('every-third');
+
+  for (let i = 0; i < wordsArray.length; i+=3) {
+    const word = wordsArray[i];
+    const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      EveryThirdElement.appendChild(wordElement);
+    
+  }
 }
+
+
 
 // TODO: Define your own functions here!
 
@@ -91,9 +119,11 @@ function displaySpeechStats() {
   displayLongWords();
 
   displayNameStart();
+
+  displayUncommonWords();
   
   displayLongestWord();
 
-  
+  EveryThird();
   // TODO: Call your functions here!
 }
