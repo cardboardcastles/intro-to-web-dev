5347204f0cf493e1bd08dab4ab2c6621f2d12724
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-03-21 01:08:45 +0000

    project 7 complete!

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,7 +1,26 @@
+var width;
+var height;
+
 function setup() {
-  createCanvas(400, 400);
+  width = 400;
+  height = 400;
+  
+  createCanvas(width, height);
+  background(200);
 }
 
-function draw() {
-  background(220);
+
+function mouseClicked() {
+  var randomX = random(0, width);
+  var randomY = random(0, height);
+  
+  ellipse(randomX, randomY, 80, 80);
+}
+
+function keyPressed() {
+  width += 10;
+  height += 10;
+  
+  resizeCanvas(width, height, true);
+  background(200);
 }
