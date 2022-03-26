3fde57b4e8d66a766fe98d7f8b2dcbb44be5394b
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-03-26 22:05:19 +0000

    tutorial complete but how do i format things aaahhhhhhhh

--- week-08-objects/script.js
+++ week-08-objects/script.js
@@ -1,17 +1,35 @@
 let Characters = [
-  {FLName: "Taako", Species: "High Elf"},
-  {FLName: "Lucretia", Species: "Human"},
-  {FLName: "Killian", Species: "Orc"}
+ 
+  {FLName: "Taako", 
+  RC: {Race: 'High Elf', Class: 'Wizard'},
+  Quote: [' "Hail and well met, my dudes!" ']
+  },
+  
+  {FLName: "Magnus", 
+   RC: {Race: 'Human', Class: 'Fighter'},
+  Quote: [' "Do good recklessly!" ']
+  },
+  
+  {FLName: "Merle", RC: {Race: 'Dwarf', Class: 'Cleric'},
+  Quote: [' "I find joy in...life!" ']},
+  
 ];
 
 for (let character of Characters) {
 
 let message = character.FLName 
   + ' from TAZ is a(n) ' 
-  + character.Species;
+  + character.RC.Race
+  + ' ' + character.RC.Class;
   
 
 document.write (message);
 document.write ('<br>');
+
+
+for (let topic of character.Quote) {
+  document.write('<br> <i> this character is known for saying' + topic + '</i> <br>' + '<br>');
+  
+}
 
 }
