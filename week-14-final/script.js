a00af3542ac1189ca9a8b42ca1bbc2c8798e4052
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-05-04 20:57:10 +0000

    name input created, title, some css for the names and buttons, and some html for the next buttons

---
+++ week-14-final/script.js
@@ -0,0 +1,31 @@
+
+function fillOutputs() {
+
+  let nameInput = document.getElementById('name-input');
+  let name = nameInput.value;
+
+  
+  let nameOutput = document.getElementById('name-output');
+  nameOutput.innerText = name;
+
+ 
+  let outputDiv = document.getElementById('output');
+  outputDiv.style.display = 'block';
+}
+
+
+
+  //what is the url of your final project?
+
+// how does your project use html?
+
+// how does your project use css?
+
+// how does your project use Javascript?
+
+// How does your project use at least one of the following:
+// Event callbacks
+// DOM manipulation
+// creating a variable
+// if statements
+// for loops
