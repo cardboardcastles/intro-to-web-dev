1a1dba65afdc84936760a521f5981098e6d4df52
Carter <98548249+cardboardcastles@users.noreply.github.com>
2022-05-05 02:06:04 +0000

    backup

--- week-14-final/script.js
+++ week-14-final/script.js
@@ -15,8 +15,22 @@ function fillOutputs() {
 
 function sayThing() {
   let noTutorial = document.getElementById('NoTutorial');
-  document.write("oh wow")
-  
+  document.write("Ah, I should have recognized you! Allow me to get rid of this mess.");
+
+}
+
+function sayOtherThing() {
+  let Tutorial = document.getElementById('Tutorial');
+  document.write("Allow me to explain, then! This is a short choose-your-own-adventure thing. Every step of the way, there will be new buttons to press, new decisions to make. To begin, click the button below! <p><button id='StartButton'>Start</button></p> ");
+
+  let StartButton = document.getElementById('start-button');
+}
+
+
+//helper function
+function show(id) {
+  const element = document.getElementById(id);
+  element.style.display = 'block';
 }
 
   //what is the url of your final project?
